1ef5b686af0d848493bff3d6d4ea9f670846e2d8
IsaiasLopezO <isaloor_@hotmail.com>
2019-10-20 13:10:56 -0500

    Empleado Asalariado v1

---
+++ Empleados/EmpleadoAsalariado.java
@@ -0,0 +1,44 @@
+public class EmpleadoAsalariado extends Empleado{
+	//private fields
+	private float salarioSemanal;
+
+	//constructor method
+	public EmpleadoAsalariado(String nombre, String apellido, String numSeguridad, float salarioSemanal){
+		super.Empleado(String nombre, String apellido, String numSeguridad);
+		this.salarioSemanal = salarioSemanal;
+	}
+
+	//Accessor for nombre
+	public String getNombre(){
+		return nombre;
+	}
+
+	//Accessor for apellido
+	public String getApellido(){
+		return nombre;
+	}
+
+	//Accessor for numSeguridad
+	public String getNumSeguridad(){
+		return nombre;
+	}
+
+
+	//Accessor for salarioSemanal
+	public String getSalarioSemanal(){
+		return salarioSemanal;
+	}
+
+	//Accessor for ingresos
+	public abstract float ingresos(float salarioSemanal){
+		this.salarioSemanal = salarioSemanal;
+		return salarioSemanal;
+
+		//Data method
+		public String toString(){
+			return "Empleado asalariado [ " +getNombre() + " " + getApellido() + ". SegSocial: " + getNumSeguridad() + ". Sueldo: " + ingresos() + "]";
+		}
+
+	}
+
+}
