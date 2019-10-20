ccc7dabd7fcb7ad8e906650b64603731acd851bf
IngArcos <raul_sky@outlook.com>
2019-10-20 15:09:38 -0500

    Overriding y calculo de ingresos

--- Empleados/EmpleadoBaseMasComision.java
+++ Empleados/EmpleadoBaseMasComision.java
@@ -3,14 +3,21 @@ public class EmpleadoBaseMasComision extends EmpleadoPorComision{
 	private float salarioBase;
 
 	//Constructor
-	public EmpleadoBaseMasComision(String nombre, String apellido, String numSeguridad, float salarioBase){
-		super.EmpleadoPorComision();
+	public EmpleadoBaseMasComision(String nombre, String apellido, String numSeguridad, int ventasTotales, float tasaComision, float salarioBase){
+		super(nombre, apellido, numSeguridad, ventasTotales, tasaComision);
 		this.salarioBase = salarioBase;
 	}
 
-	//Método toString
+	//Sobreescritura método toString
+	@Override
 	public String toString(){
 		return super.toString() + "\n\tSalario base: $"+this.salarioBase;
+	}
+
+	//Sobreescritura de metodo ingresos
+	@Override
+	public float ingresos(){
+		return super.ingresos() + this.getSalarioBase();
 	}
 
 	//setter del atributo privado
@@ -19,7 +26,7 @@ public class EmpleadoBaseMasComision extends EmpleadoPorComision{
 	}
 
 	//getter del atributo privado
-	public float getSalario(){
+	public float getSalarioBase(){
 		return this.salarioBase;
 	}
 }
