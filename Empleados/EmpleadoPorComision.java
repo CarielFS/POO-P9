440c965b33869a9c4db1813b510642ea9eca132b
IngSoto <v.ivnsto@hotmail.com>
2019-10-20 23:41:06 -0500

    EmpleadoPorComisionV1

---
+++ Empleados/EmpleadoPorComision.java
@@ -0,0 +1,20 @@
+public class EmpleadoPorComision extends Empleado{
+	private int ventasTotales;
+	private float tasaComision;
+
+	public EmpleadoPorComision(String nombre, String apellido, String numSeguridad, int ventasTotales, float tasaComision){
+		super(nombre, apellido, numSeguridad);
+		this.ventasTotales = ventasTotales;
+		this.tasaComision = tasaComision;
+	}
+
+  @Override
+  public float ingresos(){
+    return (ventasTotales*tasaComision);
+  }
+
+	@Override
+	public String toString(){
+		return super.toString();
+	}
+}
